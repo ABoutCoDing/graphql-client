29adbfc6f84ed495da0c3c682ef8819f6ff43014
devhkr <jamie@snowkiwi.com>
2020-01-18 11:35:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, Fragment} from 'react';
 
-function App() {
+import Users from './Users';
+import {gql} from 'apollo-boost';
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import AuthorizedUser from './AuthorizedUser';
+import Photos from './Photos'
+import PostPhoto from './PostPhoto'
+
+import {withApollo} from 'react-apollo';
+
+
+export const ROOT_QUERY = gql`
+    query allUsers {
+      totalUsers        
+      allUsers {...userInfo}
+      me {...userInfo}
+      allPhotos {
+            id
+            name
+            url
+      }
+    }
+
+    fragment userInfo on User {
+      githubLogin
+      name
+      avatar
+    }
+`
+
+const LISTEN_FOR_USERS = gql`
+    subscription {
+        newUser {
+            githubLogin
+            name
+            avatar
+        }
+    }
+`
+
+const LISTEN_FOR_PHOTOS = gql`
+    subscription {
+        newPhoto {
+            id
+            name
+            url
+        }
+    }
+`
+
+const App = ({client}) => {
+  useEffect(() => {
+    const listenForUsers = client
+        .subscribe({ query: LISTEN_FOR_USERS })
+        .subscribe(({ data:{newUser} }) => {
+            const data = client.readQuery({ query: ROOT_QUERY })
+            data.totalUsers += 1
+            data.allUsers = [
+                ...data.allUsers,
+                newUser
+            ]
+            client.writeQuery({ query: ROOT_QUERY, data })
+        });
+
+    const listenForPhotos = client
+        .subscribe({ query: LISTEN_FOR_PHOTOS })
+        .subscribe(({ data:{ newPhoto } }) => {
+            console.log(newPhoto);
+            const data = client.readQuery({ query: ROOT_QUERY })
+            data.totalPhotos += 1
+            data.allPhotos = [
+                ...data.allPhotos,
+                newPhoto
+            ]
+            client.writeQuery({ query: ROOT_QUERY, data })
+        });
+
+    return () => {
+      listenForUsers.unsubscribe();
+      listenForPhotos.unsubscribe();
+    }
+  }, [client]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+          <Route exact path="/" component={() =>
+              <Fragment>
+                  <AuthorizedUser />
+                  <Users />
+                  <Photos />
+              </Fragment>
+          } />
+          <Route path="/newPhoto" component={PostPhoto} />
+          <Route component={({ location }) => <h1>"{location.pathname}" not found</h1>} />
+      </Switch>
+    </BrowserRouter>  
+  )
 }
 
-export default App;
+export default withApollo(App);
